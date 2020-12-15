77a9fdab4262b9ea026abb156acb07da3fbacc29
jamrozyb <56119962+jamrozyb@users.noreply.github.com>
2020-12-15 16:02:12 +0100

    add ISSPosition

---
+++ src/ISSPosition.java
@@ -0,0 +1,59 @@
+import java.io.Serializable;
+
+/** The Issposition store and shere data from single request from page "http://api.open-notify.org/iss-now.json" */
+public class ISSPosition implements Serializable {
+
+    public static class LongitudeLatitude{
+        private float longitude;
+        private float latitude;
+
+        public float getLongitude() {
+            return longitude;
+        }
+
+        public float getLatitude() {
+            return latitude;
+        }
+
+        // for check loading json to class
+        @Override
+        public String toString() {
+            return "LongitudeLatitude{" +
+                    "longitude=" + longitude +
+                    ", latitude=" + latitude +
+                    '}';
+        }
+    }
+    private int timestamp;
+    private LongitudeLatitude iss_position;
+    private String message;
+
+    public int getTimestamp() {
+        return timestamp;
+    }
+
+    public LongitudeLatitude getIss_position() {
+        return iss_position;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public float getLongitude() {
+        return iss_position.getLongitude();
+    }
+
+    public float getLatitude() {
+        return iss_position.getLatitude();
+    }
+
+    // for check loading json to class
+    @Override
+    public String toString() {
+        return "ISSposition{" +
+                "timestamp=" + timestamp +
+                ", iss_position=" + iss_position +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
