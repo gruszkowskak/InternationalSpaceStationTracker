9af6bfe4b1b3d6e15a608e277aa61ecee557c4e6
jamrozyb <56119962+jamrozyb@users.noreply.github.com>
2020-12-16 18:19:47 +0100

    add read get from web "people in space now " and " iss pass times "

---
+++ src/PeopleInSpaceNow.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class PeopleInSpaceNow {
+    public class People{
+        private String name;
+        private String craft;
+
+        public String getName() {
+            return name;
+        }
+
+        public String getCraft() {
+            return craft;
+        }
+
+        @Override
+        public String toString() {
+            return "People{" +
+                    "name='" + name + '\'' +
+                    ", craft='" + craft + '\'' +
+                    '}';
+        }
+    }
+    private String message;
+    private Integer number;
+    private ArrayList<People> people;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public ArrayList<People> getPeople() {
+        return people;
+    }
+
+    @Override
+    public String toString() {
+        return "PeopleInSpaceNow{" +
+                "message='" + message + '\'' +
+                ", number=" +number +
+                ", people=" + people +
+                '}';
+    }
+}
