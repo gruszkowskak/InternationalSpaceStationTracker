4c437c2707fbdf14f195b0773b432c94f78905cf
klagru <klaudia.gruszkowska.stud@pw.edu.pl>
2020-12-17 12:55:19 +0100

    return longitude and latitude on MainPanel

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,14 +1,24 @@
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 public class MainFrame extends JFrame {
 
-    public MainFrame() {
+    public MainFrame() throws IOException, InterruptedException {
         super("ISS tracker");
+        setLayout(new FlowLayout());
 
         JPanel worldMapPanel = new WorldMapPanel();
         add(worldMapPanel);
+
+
+        // Create HttpClient once in program
+        ISSPositionURL issPositionURL= new ISSPositionURL();
+        // get actual issPosition, use when you nid get new actual position
+        ISSPosition issPosition = issPositionURL.RequestISSPosition();
+        add(new JTextField("Latitude is : "+String.valueOf(issPosition.getLatitude())));
+        add(new JTextField("Longitude is : "+String.valueOf(issPosition.getLongitude())));
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
--- src/StartApp.java
+++ src/StartApp.java
@@ -1,11 +1,18 @@
 import java.awt.EventQueue;
+import java.io.IOException;
 
 public class StartApp {
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new MainFrame();
+                try {
+                    new MainFrame();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
