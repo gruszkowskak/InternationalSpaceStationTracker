b901ffd2cae6732ff68e64142cf05a667acfafb5
ja <ja@asred.pl>
2020-12-20 20:05:52 +0100

    added astronauts and pass time frames

---
+++ src/Astronauts_Frame.java
@@ -0,0 +1,37 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Astronauts_Frame extends JFrame {
+
+    public Astronauts_Frame() throws IOException, InterruptedException {
+        super("Astronauts");
+        setLayout(new FlowLayout());
+
+
+        PeopleInSpaceNowURL peopleInSpaceNowURL= new PeopleInSpaceNowURL();
+
+        PeopleInSpaceNow peopleInSpaceNow = peopleInSpaceNowURL.RequestPeopleInSpaceNow();
+        ArrayList people = peopleInSpaceNow.getPeople();
+
+        Pattern pattern = Pattern.compile("'(.*?)'");
+        for (int i=0; i<people.size(); i++) {
+            String mydata = people.get(i).toString();
+            //find name of astronaut
+            Matcher matcher = pattern.matcher(mydata);
+            String name="a";
+            if (matcher.find())
+            {
+                name = matcher.group(1);
+            }
+            add(new JLabel(i+1+"." + name+"\n"));
+        }
+
+        pack();
+        setVisible(true);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import javax.swing.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -11,5 +15,28 @@ public class Main {
         System.out.println(issPosition);
         System.out.println(issPosition.getLatitude() + " "+ issPosition.getLongitude());
 
+
+        // Create HttpClient once in program
+        ISSPassTimesURL isspasstimesURL= new ISSPassTimesURL();
+        // get actual issPosition, use when you nid get new actual position
+        ISSPassTimes issPassTimes = isspasstimesURL.RequestISSPassTimes(52.216f, 21);
+        System.out.println(issPassTimes.getMessage());
+        System.out.println(issPassTimes.getResponse());
+        System.out.println(issPassTimes.getResponse().get(0));
+        System.out.println(issPassTimes.getRequest());
+
+        // Create HttpClient once in program
+        PeopleInSpaceNowURL peopleInSpaceNowURL= new PeopleInSpaceNowURL();
+        // get actual issPosition, use when you nid get new actual position
+        PeopleInSpaceNow peopleInSpaceNow = peopleInSpaceNowURL.RequestPeopleInSpaceNow();
+        ArrayList people = peopleInSpaceNow.getPeople();
+        String mydata = people.get(1).toString();
+        Pattern pattern = Pattern.compile("'(.*?)'");
+        Matcher matcher = pattern.matcher(mydata);
+        if (matcher.find())
+        {
+            System.out.println(matcher.group(1));
+        }
+        System.out.println(people.get(1).toString());
     }
 }
