5efef3345ae796b18fe663d64b978c3d0daa5176
ja <ja@asred.pl>
2021-01-05 12:49:28 +0100

    added astronauts and pass time frames2

--- src/Astronauts_Frame.java
+++ src/Astronauts_Frame.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
+import java.awt.event.ActionEvent;
 import java.util.regex.Pattern;
 
 public class Astronauts_Frame extends JFrame {
@@ -11,6 +12,40 @@ public class Astronauts_Frame extends JFrame {
     public Astronauts_Frame() throws IOException, InterruptedException {
         super("Astronauts");
         setLayout(new FlowLayout());
+        JMenu mapMenu = new JMenu("Maps");
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(mapMenu);
+        JMenu astrounautsMenu = new JMenu("Astrounauts");
+        JMenuItem astrounatsItem = new JMenuItem("Go to...");
+        astrounatsItem.addActionListener((event)-> {
+            try {
+                new Astronauts_Frame();
+                setVisible(true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            this.setVisible(false);
+        });
+        astrounautsMenu.add(astrounatsItem);
+        menuBar.add(astrounautsMenu);
+        JMenu pass_timeMenu = new JMenu("Pass Time");
+        JMenuItem pass_timeItem = new JMenuItem("Go to...");
+        pass_timeItem.addActionListener((event)-> {
+            try {
+                new PassTime_Frame();
+                setVisible(true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            this.setVisible(false);
+        });
+        pass_timeMenu.add(pass_timeItem);
+        menuBar.add(pass_timeMenu);
+        setJMenuBar(menuBar);
 
 
         PeopleInSpaceNowURL peopleInSpaceNowURL= new PeopleInSpaceNowURL();
