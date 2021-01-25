4d18e21887ea13bf96f902e6fa08d7c28563fe74
jamrozyb <56119962+jamrozyb@users.noreply.github.com>
2021-01-25 16:41:40 +0100

    auto creat dir for astronaut photo

--- src/main/java/gui/Astronauts_Frame.java
+++ src/main/java/gui/Astronauts_Frame.java
@@ -9,7 +9,6 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
